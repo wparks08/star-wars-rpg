00c7cc6877e42e98ffc9a73a3f0e2e4678488a0a
Will Parks <wparks08@gmail.com>
2019-10-01 23:36:11 -0700

    character class added and started implementing game object

--- assets/js/character.js
+++ assets/js/character.js
@@ -1,2 +1,24 @@
 //character constructor lives here
-//-- healthPoints, baseAttackPower, attackPower, counterAttackPower, image, name
+//-- healthPoints, baseAttackPower, attackPower, counterAttackPower, imageUrl, name
+class Character {
+    constructor(healthPoints, baseAttackPower, counterAttackPower, imageUrl, name) {
+        this.healthPoints = healthPoints;
+        this.baseAttackPower = baseAttackPower;
+        this.attackPower = baseAttackPower;
+        this.counterAttackPower = counterAttackPower;
+        this.imageUrl = imageUrl;
+        this.name = name;
+    }
+
+    reduceHp(damage) {
+        this.healthPoints -= damage;
+    }
+
+    increaseAttackPower() {
+        this.attackPower += this.baseAttackPower;
+    }
+
+    generateImgTag() { //Not sure if I'll need this w/ jQuery? Delete later if unused.
+        return "<img src='" + this.imageUrl + "' alt='" + this.name + "'>";
+    }
+};
--- assets/js/game.js
+++ assets/js/game.js
@@ -2,51 +2,82 @@
 //-- healthPoints, baseAttackPower, attackPower, counterAttackPower, image, name
 
 //Organize game in object
+var game = {
+    //Array storing 4 Character objects
+    characters: [],
+    //Selected Character
+    userCharacter: Character,
+    //Selected enemy
+    selectedEnemy: Character,
+    //[Array] Enemy Characters
+    allEnemies: [],
+    //Game message
+    message: "",
 
-//Array storing 4 Character objects
-//Selected Character
-//Selected enemy
-//[Array] Enemy Characters
-//Game message
+    initialize: function() {
+        //-- create character objects, store them in allCharacters array
+        //-- update display() [leave this up to jQuery?]
+        this.characters.push(
+            new Character(150, 10, 15, "placeholderImage", "Obi Wan"),
+            new Character(160, 5, 10, "placeholderImage", "Luke"),
+            new Character(175, 15, 20, "placeholderImage", "Kylo Ren"),
+            new Character(190, 20, 25, "placeholderImage", "Rey")
+        )
 
-//initialize
-//-- create character objects, store them in allCharacters array
-//-- update display()
+        console.log(this.characters);
+    },
 
-//Select Character method
-//-- Clicking a character selects that character for the player to use
-//-- All other characters moved into enemies array
+    //Select Character method
+    //-- Clicking a character selects that character for the player to use
+    //-- All other characters moved into enemies array
+    selectCharacter: function(index) {
+        this.userCharacter = this.characters[index];
+        for (let i = 0; i < this.characters.length; i++) {
+            if (!(this.userCharacter === this.characters[i])) {
+                this.allEnemies.push(this.characters[i]);
+            }
+        }
+    },
 
-//Select Enemy method
-//-- Clicking a character selects that character for the player to fight
+    //Select Enemy method
+    //-- Clicking a character selects that character for the player to fight
 
-//Attack method
-//-- Player moves first
-//-- subtract user character's attackPower from enemy character's healthPoints
-//-- increase user character's attackPower by baseAttackPower
-//-- check game status()
-//-- subtract enemy character's attackPower from user character's healthPoints
-//-- check game status()
+    //Attack method
+    //-- Player moves first
+    //-- subtract user character's attackPower from enemy character's healthPoints
+    //-- increase user character's attackPower by baseAttackPower
+    //-- check game status()
+    //-- subtract enemy character's attackPower from user character's healthPoints
+    //-- check game status()
 
-//calculateMove(attacker, defender)
-//-- subtract attacker.attackPower from defender.healthPoints
+    //calculateMove(attacker, defender)
+    //-- subtract attacker.attackPower from defender.healthPoints
 
-//checkGameStatus
-//-- if user is still alive, allow enemy selection
-//-- if user is not alive, game over
-//-- updateDisplay
+    //checkGameStatus
+    //-- if user is still alive, allow enemy selection
+    //-- if user is not alive, game over
+    //-- updateDisplay [leave this up to jQuery?]
 
-//updateDisplay(options)
-//-- update allCharacters
-//-- update userCharacter - should update HP also
-//-- update enemyCharacters
-//-- update currentEnemy - should update HP also
-//-- update gameMessage = options.message || ""
-
+    //updateDisplay(options)
+    //-- update allCharacters
+    //-- update userCharacter - should update HP also
+    //-- update enemyCharacters
+    //-- update currentEnemy - should update HP also
+    //-- update gameMessage = options.message || ""
+}
 
 //jQuery events
 
 //initialize page on load
+$(document).ready(function() {
+    game.initialize();
+    $.each(game.characters, function( index, character ) {
+        var characterElement = $("<li>").html(`Name: ${character.name} HP: ${character.healthPoints}`);
+        characterElement.attr("data-index", index);
+        characterElement.addClass("character");
+        $("#character-list").append(characterElement);
+    })
+});
 //select character on click
 //select enemy on click
 //attack on click
