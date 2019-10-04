306e6f0a0a8d2dfe67333c0970a02b1a59ef5b0e
Will Parks <wparks08@gmail.com>
2019-10-03 22:09:06 -0700

    implementing game logic and display updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 //Define Character object
 //-- healthPoints, baseAttackPower, attackPower, counterAttackPower, image, name
+var gameContainer = $("<div>");
+gameContainer.addClass("container");
+var gameMessageElement = $("<div id='message'>");
+var characterListElement = $("<ul id='character-list'>");
+var selectedCharacterElement = $("<div id='selected-character'>");
+var availableEnemiesElement = $("<div id='available-enemies'>");
+var fightSectionElement = $("<div id='fight-section'>");
+var currentEnemyElement = $("<div id='current-enemy'>");
 
 //Organize game in object
 var game = {
@@ -7,10 +15,12 @@ var game = {
     characters: [],
     //Selected Character
     userCharacter: Character,
+    userHasSelectedCharacter: false,
     //Selected enemy
     selectedEnemy: Character,
+    fighting: false,
     //[Array] Enemy Characters
-    allEnemies: [],
+    availableEnemies: [],
     //Game message
     message: "",
 
@@ -24,23 +34,39 @@ var game = {
             new Character(190, 20, 25, "placeholderImage", "Rey")
         )
 
-        console.log(this.characters);
+        this.updateDisplay({ message: "Choose a character to play as!" });
     },
 
     //Select Character method
     //-- Clicking a character selects that character for the player to use
     //-- All other characters moved into enemies array
     selectCharacter: function(index) {
-        this.userCharacter = this.characters[index];
-        for (let i = 0; i < this.characters.length; i++) {
-            if (!(this.userCharacter === this.characters[i])) {
-                this.allEnemies.push(this.characters[i]);
+        if (!this.userHasSelectedCharacter) {
+            this.userCharacter = this.characters[index];
+            for (let i = 0; i < this.characters.length; i++) {
+                if (!(this.userCharacter === this.characters[i])) {
+                    this.availableEnemies.push(this.characters[i]);
+                }
             }
         }
+
+        this.characters = [];
+        this.userHasSelectedCharacter = true;
+        
+       this.updateDisplay({ message: "Choose an enemy!" });
     },
 
     //Select Enemy method
     //-- Clicking a character selects that character for the player to fight
+    selectEnemy: function(index) {
+        if (!this.fighting) {
+            this.selectedEnemy = this.availableEnemies[index];
+            this.fighting = true;
+            this.availableEnemies.splice(index, 1);
+        }
+
+        this.updateDisplay({ message: "Click Fight to Attack!" });
+    },
 
     //Attack method
     //-- Player moves first
@@ -49,6 +75,9 @@ var game = {
     //-- check game status()
     //-- subtract enemy character's attackPower from user character's healthPoints
     //-- check game status()
+    attack: function() {
+        console.log("attacked!");
+    },
 
     //calculateMove(attacker, defender)
     //-- subtract attacker.attackPower from defender.healthPoints
@@ -64,20 +93,93 @@ var game = {
     //-- update enemyCharacters
     //-- update currentEnemy - should update HP also
     //-- update gameMessage = options.message || ""
+    updateDisplay: function(options) {
+        options = Object.assign(
+            {
+                message: ""
+            },
+            options
+        )
+        this.updateCharacterList();
+        this.updateUserCharacter();
+        this.updateEnemyCharacters();
+        this.updateCurrentEnemy();
+        this.updateFightSection();
+        this.updateGameMessage(options.message);
+    },
+
+    updateCharacterList: function() {
+        characterListElement.empty();
+        $.each(this.characters, function (index, character) {
+            var characterElement = character.generateCharacterElement();
+            characterElement.attr("data-index", index);
+            characterListElement.append(characterElement);
+        });
+    },
+
+    updateUserCharacter: function() {
+        if (this.userHasSelectedCharacter) {
+            let selectedCharacter = this.userCharacter;
+            selectedCharacterElement.empty()
+                .html(selectedCharacter.generateCharacterElement());
+        }
+    },
+
+    updateEnemyCharacters: function() {
+        availableEnemiesElement.empty();
+        $.each(this.availableEnemies, function(index, character) {
+            var characterElement = character.generateCharacterElement();
+            characterElement.attr("data-index", index);
+            availableEnemiesElement.append(characterElement);
+        })
+
+        $("#available-enemies .character").on("click", function () {
+            game.selectEnemy($(this).data("index"));
+        });
+    },
+
+    updateCurrentEnemy: function() {
+        if (this.fighting) {
+            let currentEnemy = this.selectedEnemy;
+            currentEnemyElement.empty()
+                .html(currentEnemy.generateCharacterElement());
+        }
+    },
+
+    updateFightSection: function() {
+        let attackMethod = this.attack;
+        fightSectionElement.empty();
+        if (this.fighting) {
+            let fightButton = $("<button>");
+            fightButton.html("Fight!");
+            fightSectionElement.append(fightButton);
+
+            fightButton.on("click", function () {
+                attackMethod();
+            });
+        }
+    },
+
+    updateGameMessage: function(message) {
+        gameMessageElement.empty()
+            .html(`<p>${message}</p>`);
+    }
 }
 
 //jQuery events
 
 //initialize page on load
 $(document).ready(function() {
+    $("body").prepend(gameContainer);
+    gameContainer.append([gameMessageElement, characterListElement, selectedCharacterElement, availableEnemiesElement, fightSectionElement, currentEnemyElement]);
+
     game.initialize();
-    $.each(game.characters, function( index, character ) {
-        var characterElement = $("<li>").html(`Name: ${character.name} HP: ${character.healthPoints}`);
-        characterElement.attr("data-index", index);
-        characterElement.addClass("character");
-        $("#character-list").append(characterElement);
-    })
+
+    $("#character-list .character").on("click", function() {
+        game.selectCharacter($(this).data("index"));
+    });
+
+    
 });
-//select character on click
 //select enemy on click
 //attack on click
